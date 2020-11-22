06ddecb884238cf805c3e885bbd659118c2a25d2
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2020-11-21 20:02:55 -0500

    change table name of migrations and validations

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    protected $table = 'user';
+
     /**
      * The attributes that should be hidden for arrays.
      *
